c17f4a4fd681fb7a8219330a9004ad75d57774e3
alpersimsek1 <alpersim94@gmail.com>
2025-10-19 19:34:02 +0100

    still not yet

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
           <ImageCarousel />
         </div>
 
-        <div className="absolute bottom-0 left-0 w-full h-32 md:h-48 lg:h-64 z-20">
+        <div className="absolute bottom-0 left-0 w-full h-32 md:h-48 lg:h-64 z-20 hidden md:block">
           <svg viewBox="0 0 1440 320" className="w-full h-full" preserveAspectRatio="none">
             <path fill="#ffffff" fillOpacity="1" d="M0,96L48,112C96,128,192,160,288,160C384,160,480,128,576,122.7C672,117,768,139,864,138.7C960,139,1056,117,1152,112C1248,107,1344,117,1392,122.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           </svg>
